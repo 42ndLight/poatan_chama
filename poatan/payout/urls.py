6b05f0bfc976f6f19765eda0cd5afff573d9767a
42ndLight <leichteed@gmail.com>
2025-04-01 01:53:59 +0300

    fixed urls in payout

--- poatan/payout/urls.py
+++ poatan/payout/urls.py
@@ -4,7 +4,6 @@ from .views import PayoutView, PayoutDetailView, ProcessPayoutView
 
 urlpatterns = [
     path('', PayoutView.as_view(), name='payout-list'),
-     path('new/', PayoutView.as_view(), name='payout-create'),
     path('<int:pk>/', PayoutDetailView.as_view(), name='payout-detail'),
     path('<int:pk>/process/', ProcessPayoutView.as_view(), name='process-payout'),
 ]
